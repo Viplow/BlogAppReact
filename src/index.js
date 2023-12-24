f22e4c478385fb6eec7e9288b05e5e92396cc891
Viplow Srivastava <viplow.srivastava@indiamart.com>
2023-12-24 18:51:46 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
+import reportWebVitals from "./reportWebVitals";
+import "./app.css";
+import { getBlogsFromDatabase, clearBlogs } from "./actions/blogs";
+import "./firebase/firebaseConfig";
+import { onAuthStateChanged, getAuth } from "./firebase/firebaseConfig";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { loginAction, logoutAction } from "./actions/auth";
+import store from "./store/configureStore";
+
+store.subscribe(()=>{
+  console.log("===== Store Changed ====");
+  console.log("Auth state: ", store.getState().auth);
+  console.log("Blogs state: ", store.getState().blogs);
+  console.log("------------------------");
+});
+
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log("User Login: ", user.uid);
+        store.dispatch(loginAction(user.uid));
+
+        store.dispatch(getBlogsFromDatabase()).then(() => {
+            renderApp();
+        })
+    } else {
+        store.dispatch(clearBlogs());
+        store.dispatch(logoutAction());
+        console.log("User Logout.");
+        renderApp();
+    }
+});
+
+let isRendered = false;
+const renderApp = () => {
+    if(!isRendered) {
+        ReactDOM.render(
+            <Provider store={store}>
+                <AppRouter />
+            </Provider>, 
+            document.getElementById('root')
+        );
+        isRendered = true;
+    }
+}
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
